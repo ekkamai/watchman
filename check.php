389db3a0ced9467f990f28491563f9ffb8ce657f
ekkamai <peppy@Peppys-MacBook-Pro.local>
2017-05-04 20:22:06 +0700

    Updated key

--- check.php
+++ check.php
@@ -4,23 +4,23 @@ error_reporting(E_ALL ^ E_NOTICE); // hide site wide notices
 /**
  * Malware Alert
  * A simple tool that checks the "malware" status of a domain in the Google safebrowsing API
- * 
- *  __  __       _                             _    _           _   
- * |  \/  | __ _| __      ____ _ _ __ ___     / \  | | ___ _ __| |_ 
+ *
+ *  __  __       _                             _    _           _
+ * |  \/  | __ _| __      ____ _ _ __ ___     / \  | | ___ _ __| |_
  * | |\/| |/ _` | \ \ /\ / / _` | '__/ _ \   / _ \ | |/ _ | '__| __|
- * | |  | | (_| | |\ V  V | (_| | | |  __/  / ___ \| |  __| |  | |_ 
- * |_|  |_|\__,_|_| \_/\_/ \__,_|_|  \___| /_/   \_|_|\___|_|   \__|    
+ * | |  | | (_| | |\ V  V | (_| | | |  __/  / ___ \| |  __| |  | |_
+ * |_|  |_|\__,_|_| \_/\_/ \__,_|_|  \___| /_/   \_|_|\___|_|   \__|
  *
  *
  * @author     Nick Peplow
  * @version    1
  * @package    malware-alert
- * @link       https://github.com/ekkamai/malware-alert
+ * @link       https://github.com/ekkamai/watchman
  */
 
 /** @apikey you need to register for a google api key and update it below */
 
-	$api_key = "AIzaSyAh3Po5slsBi7P-piZTxnHCIkkfd9ddH0k"; // e.g. XXXXXXXXXXXXXX-XXXXXXXXXXXX
+	$api_key = "XXXXXXXXXXXXXX-XXXXXXXXXXXX"; // e.g. XXXXXXXXXXXXXX-XXXXXXXXXXXX
 
 /** @checks are the variables set correctly? */
 
@@ -31,14 +31,14 @@ error_reporting(E_ALL ^ E_NOTICE); // hide site wide notices
 		if (empty($chk_url)) {
 			exit("<h2>Domain not included in URL</h2>You must include it as a paramater in the url e.g. /check.php?url=ianfette.org");
 		}
-		
+
 	// was the api key set?
 		if (empty($api_key)) {
 			exit("<h2>API key has not been set</h2>You must include it as a paramater in the url e.g. /check.php?url=ianfette.org");
 		}
 
 /** @sendpostdata the primary function that queries the api, along with the request content */
-	
+
 	// set the google api query url
 	$api_url ="https://safebrowsing.googleapis.com/v4/threatMatches:find?key=".$api_key."";
 
@@ -51,11 +51,11 @@ error_reporting(E_ALL ^ E_NOTICE); // hide site wide notices
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $api_data);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$result = curl_exec($ch);
-	
+
 	// return the result
 		return $result;
 	}
-	
+
 	// compose our request
 	$api_data = "{
 	  'client': {
@@ -76,36 +76,36 @@ error_reporting(E_ALL ^ E_NOTICE); // hide site wide notices
 
 	//run the API query
 		$api_result = sendPostData($api_url, $api_data);
-		
+
 	//decode the json response
 		$responseData = json_decode($api_result, TRUE);
-		
+
 	//find threat type
 		$threat = $responseData["matches"][0]["threatType"];
-		
+
 		//was a threat type returned
 		if (isset($threat))
 			{
 			// did it match a threat type?
 			if ($threat == 'MALWARE') //malware?
 				{
-				$status = "STATUS:MALWARE";
+				$status = "MALWARE";
 				}
 			elseif ($threat == 'SOCIAL_ENGINEERING') //social engineering?
 				{
-				$status = "STATUS:SOCIAL_ENGINEERING";
+				$status = "SOCIAL_ENGINEERING";
 				}
 			}
 		  else
 			{
-			$status = "STATUS:CLEAN"; //didnt return with a threat, so lets assume its clean
-			}  
-		
-		//echo out the status	
-		echo $status;	
-		
-/** @logs primary function that queries */		
-	
+			$status = "CLEAN"; //didnt return with a threat, so lets assume its clean
+			}
+
+		//echo the status
+		echo $status;
+
+/** @logs primary function that queries */
+
 	//compose our log file contents
 	$log  = "Domain: ".$chk_url.PHP_EOL.
 			"Time: ".date("F j, Y, g:i a").PHP_EOL.
@@ -113,14 +113,14 @@ error_reporting(E_ALL ^ E_NOTICE); // hide site wide notices
 	        "-------------------------".PHP_EOL;
 	//Save results to log
 	file_put_contents('logs/log_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
-	
+
 /** @debug fark, lets figure out what's going wrong. displays the full repsonse from google */
 
 
-	
+
 	// display the JSON dump
 		//	echo "<pre>";
 		//	var_dump($responseData);
 		//	echo "</pre>";
-	
+
 ?>
